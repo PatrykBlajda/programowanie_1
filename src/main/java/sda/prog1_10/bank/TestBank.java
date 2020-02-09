b44b18c67d90d244313818ecf3d8bf83ec05c384
Patryk <patrykblajda@gmail.com>
2020-02-09 13:18:58 +0100

    TestBank

---
+++ src/main/java/sda/prog1_10/bank/TestBank.java
@@ -0,0 +1,23 @@
+package sda.prog1_10.bank;
+
+public class TestBank {
+    public static void main(String[] args) {
+        Bank bank = new Bank ("SDA BANK");
+        Customer customer1 = new Customer("Jan", "Kowalski");
+        Customer customer2 = new Customer("Maria", "Wróbel");
+        Customer customer3 = new Customer("Danuta", "Więcek");
+
+        bank.addCustomer(customer1);
+        bank.addCustomer(customer2);
+        bank.addCustomer(customer3);
+
+        bank.addAccount(customer1, AccountKind.CURRENT);
+        bank.addAccount(customer1, AccountKind.SAVINGS);
+        bank.addAccount(customer2, AccountKind.CURRENT);
+        bank.addAccount(customer2, AccountKind.SAVINGS);
+        bank.addAccount(customer3, AccountKind.CURRENT);
+        bank.addAccount(customer3, AccountKind.SAVINGS);
+
+
+    }
+}
